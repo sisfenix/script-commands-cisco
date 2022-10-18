9dfee7b9d5f3a26ea2f5fdc1e8d25234b28b7349
Alan Lopes <desenv.alanlopes@gmail.com>
2022-10-18 11:57:54 -0300

    chg: alteração para ler de arquivo csv

--- script-commands-cisco.py
+++ script-commands-cisco.py
@@ -4,30 +4,36 @@ import getpass
 import os
 import csv
 
-#ip_address = input("IP Address: ")
 #ssh_username = input("SSH Username: ")
 #ssh_password = getpass.getpass('SSH Password: ')
+#ssh_command = input("Command: ")
 
-ip_address = os.environ["NETMIKO_IP"]
 ssh_username = os.environ["NETMIKO_USER"]
 ssh_password = os.environ["NETMIKO_PASS"]
-
+ssh_command = os.environ["NETMIKO_CMD"]
 
 def main():
-    ssh_connect = netmiko.ConnectHandler(
-        device_type="cisco_ios",
-        ip=ip_address,
-        username=ssh_username,
-        password=ssh_password
-    )
+    with open("file.txt", "r") as filecsv:
+        readerfile = csv.DictReader(filecsv)
 
-    result = ssh_connect.find_prompt() + "\n"
-    ssh_connect.send_command("terminal length 0")
-    result += ssh_connect.send_command("show ip arp", read_timeout=120)
+        for row in readerfile:
+            ssh_connect = netmiko.ConnectHandler(
+            device_type="cisco_ios",
+            ip=row["ipaddress"],
+            username=ssh_username,
+            password=ssh_password
+            )
+
+            ssh_connect.send_command("terminal length 0")
+
+            result = "IP: " + row['ipaddress'] + " - S/N: " + row['sn'] + "\n"
+            result += ssh_connect.find_prompt() + "\n"
+            result += ssh_connect.send_command(ssh_command, read_timeout=120)
     
-    ssh_connect.disconnect()
+            ssh_connect.disconnect()
 
-    return result
+            with open("output/" + row['ipaddress'] + ".txt", "w") as fw:
+                print(result, file=fw)
 
 if __name__ == "__main__":
-    print(main())
+    main()
